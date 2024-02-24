547d0cb6cb9a487d0dd4f5f13783f4ece1c282a6
zunniga <jmoficial2003@gmail.com>
2024-02-24 15:02:21 -0500

    change important

--- components/homeContent/Presentation.jsx
+++ components/homeContent/Presentation.jsx
@@ -11,7 +11,7 @@ const Presentation = () => {
       <div className="mt-4 text-neutral-500 text-center">
         <div className="text-base font-semibold sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl animate__animated animate__backInLeft">
          <p className='text-primaryblue lg:text-2xl'>Somos una empresa que brinda un servicio de </p>
-         <p className='text-primaryrosa lg:text-2xl'>especializacion para empresas, personas y especialistass Hitler.</p>
+         <p className='text-primaryrosa lg:text-2xl'>especializacion para empresas, personas y especialistas.</p>
         </div>
       </div>
     </div>
