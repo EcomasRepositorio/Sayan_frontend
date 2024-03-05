24a29309b5a0065cc2e8923b88f4f8da5288f56f
zunniga <jmoficial2003@gmail.com>
2024-03-05 15:59:38 -0500

    information of courses and graduates

--- components/utils/courses/course.ts
+++ components/utils/courses/course.ts
@@ -11,3 +11,189 @@ export const coursesAlimentary = [
     "Trazabilidad y etiquetado",
     "Auditoria del Sistema de Gestión de Calidad e Inocuidad Alimentaria en base a la Norma ISO 19011"
 ];
+
+
+
+  /* INGENIERIA CIVIL */
+
+export const coursesAsistenteTecnico = [
+    {name: "Planificación y programación de obras", text:"Hola, deseo más información sobre el curso de Planificación y programación de obras"},
+    {name: "Control de obras", text:"Hola, deseo más información sobre el curso de Control de obras"},
+    {name: "Metrados en edificaciones", text:"Hola, deseo más información sobre el curso de Metrados en edificaciones"},
+    {name: "Cuaderno de obra físico y digital", text:"Hola, deseo más información sobre el curso de Cuaderno de obra físico y digital"},
+    {name: "Costos y Presupuestos con S10", text:"Hola, deseo más información sobre el curso de Costos y Presupuestos con S10"},
+    {name: "Valorización de obras", text:"Hola, deseo más información sobre el curso de Valorización de obras"},
+    {name: "Liquidación de obras", text:"Hola, deseo más información sobre el curso de Liquidación de obras"},
+    {name: "Formulación y planificación de proyectos con MS Project", text:"Hola, deseo más información sobre el curso de formulación y planificación de proyectos con MS Project"},
+    {name: "Análisis y diseño de instituciones educativas", text:"Hola, deseo más información sobre el curso de Análisis y diseño de instituciones educativas"},
+    {name: "Seguridad, salud ocupacional y medio ambiente en obras públicas", text:"Hola, deseo más información sobre el curso de Seguridad, salud ocupacional y medio ambiente en obras públicas"},
+    {name: "Expediente técnico en obras", text:"Hola, deseo más información sobre el curso de Expediente técnico en obras"},
+    {name: "Régimen laboral en construcción civil", text:"Hola, deseo más información sobre el curso de Régimen laboral en construcción civil"},
+   
+     
+];
+
+export const coursesIngenieriaVial = [
+    {name: "Geología – Geotecnia en obras viales", text:"Hola, deseo más información sobre el curso de Geología – Geotecnia en obras viales"},
+    {name: "Suelos y pavimentos", text:"Hola, deseo más información sobre el curso de Suelos y pavimentos"},
+    {name: "Hidrología e hidráulica", text:"Hola, deseo más información sobre el curso de Hidrología e hidráulica"},
+    {name: "Estructuras en carreteras - puentes", text:"Hola, deseo más información sobre el curso de Estructuras en carreteras - puentes"},
+    {name: "Metrados, costos y presupuesto – cronograma en obras viales", text:"Hola, deseo más información sobre el curso de Metrados, costos y presupuesto – cronograma en obras viales"},
+    {name: "Arqueología en proyectos viales", text:"Hola, deseo más información sobre el curso de Arqueología en proyectos viales"},
+    {name: "Estudio medio ambiental – derecho de vía – interferencias", text:"Hola, deseo más información sobre el curso de Estudio medio ambiental – derecho de vía – interferencias"},
+    {name: "Diseño, construcción y mantenimiento de Túneles", text:"Hola, deseo más información sobre el curso de Diseño, construcción y mantenimiento de Túneles"},
+    {name: "Integración de proyectos de infraestructura vial", text:"Hola, deseo más información sobre el curso de Integración de proyectos de infraestructura vial"},
+    {name: "Tráfico y carga", text:"Hola, deseo más información sobre el curso de Tráfico y carga"},
+    {name: "Topografía, diseño geométrico, señalización y seguridad vial", text:"Topografía, diseño geométrico, señalización y seguridad vial"},
+ 
+];
+
+
+
+export const coursesIngenieriaPuentes = [
+    {name: "Diseño Sísmico en puentes AASHTO-LRFD", text:"Hola, deseo más información sobre el curso de Diseño Sísmico en puentes AASHTO-LRFD"},
+    {name: "Mantenimiento y conservacion de puentes", text:"Hola, deseo más información sobre el curso de Mantenimiento y conservacion de puentes"},
+    {name: "Inspeccion, Evaluación de puentes bajo la metodología SCAP", text:"Hola, deseo más información sobre el curso de Inspeccion, Evaluación de puentes bajo la metodología SCAP"},
+    {name: "Diseño de puentes, pontones", text:"Hola, deseo más información sobre el curso de Diseño de puentes, pontones"},
+    {name: "Estudios de Hidrología e hidráulica en puentes", text:"Hola, deseo más información sobre el curso de Estudios de Hidrología e hidráulica en puentes"},
+    {name: "Estudios Geológicos y geotécnicos en puentes", text:"Hola, deseo más información sobre el curso de Estudios Geológicos y geotécnicos en puentes"},
+    {name: "Diseño y modelamiento de puentes con SAP 2000", text:"Hola, deseo más información sobre el curso de Diseño y modelamiento de puentes con SAP 2000"},
+    {name: "Diseño y modelamiento  de puentes con CSi BRIDGE", text:"Hola, deseo más información sobre el curso de Diseño y modelamiento  de puentes con CSi BRIDGE"},
+  
+
+];
+
+export const coursesAsistenteViales = [
+    {name: "Hidrología e hidráulica", text:"Hola, deseo más información sobre el curso de Hidrología e hidráulica"},
+    {name: "Estructuras en carreteras - puentes", text:"Hola, deseo más información sobre el curso de Estructuras en carreteras - puentes"},
+    {name: "Metrados, costos y presupuesto – cronograma en obras viales", text:"Hola, deseo más información sobre el curso de Metrados, costos y presupuesto – cronograma en obras viales"},
+    {name: "Arqueología en proyectos viales", text:"Hola, deseo más información sobre el curso de Arqueología en proyectos viales"},
+    {name: "Estudio medio ambiental – derecho de vía – interferencias", text:"Hola, deseo más información sobre el curso de Estudio medio ambiental – derecho de vía – interferencias"},
+    {name: "Diseño, construcción y mantenimiento de Túneles", text:"Hola, deseo más información sobre el curso de Diseño, construcción y mantenimiento de Túneles"},
+    {name: "Integración de proyectos de infraestructura vial", text:"Hola, deseo más información sobre el curso de Integración de proyectos de infraestructura vial"},
+    {name: "Tráfico y carga", text:"Hola, deseo más información sobre el curso de Integración de Tráfico y carga"},
+    {name: "Topografía, diseño geométrico, señalización y seguridad vial", text:"Hola, deseo más información sobre el curso de Topografía, diseño geométrico, señalización y seguridad vial"},
+    {name: "Geología – Geotecnia en obras viales", text:"Hola, deseo más información sobre el curso de Geología – Geotecnia en obras viales"},
+    {name: "Suelos y pavimentos", text:"Hola, deseo más información sobre el curso de Suelos y pavimentos"},
+   
+   
+];
+
+/* INGENIERIA AMBIENTAL */
+
+export const coursesEstudioAmbiental = [
+    {name: "Elaboración de mapas temáticos con ArcGIS ", text:"Hola, deseo más información sobre el curso de Elaboración de mapas temáticos con ArcGIS  "},
+    {name: "Participación ciudadana", text:"Hola, deseo más información sobre el curso de Participación ciudadana"},
+    {name: "Supervisión, Fiscalización y auditoría ambiental", text:"Hola, deseo más información sobre el curso de Supervisión, Fiscalización y auditoría ambiental"},
+    {name: "Elaboración de informe de EIA", text:"Hola, deseo más información sobre el curso de Elaboración de informe de EIA"},
+    {name: "Legislación ambiental", text:"Hola, deseo más información sobre el curso de Legislación ambiental"},
+    {name: "Instrumentos de gestión ambiental", text:"Hola, deseo más información sobre el curso de Instrumentos de gestión ambiental"},
+    {name: "Línea base de estudio de impacto ambiental", text:"Hola, deseo más información sobre el curso de Integración de Línea base de estudio de impacto ambiental"},
+    {name: "Identificación y evaluación de impacto ambiental", text:"Hola, deseo más información sobre el curso de Identificación y evaluación de impacto ambiental"},
+    {name: "Estrategias de manejo ambiental", text:"Hola, deseo más información sobre el curso de Estrategias de manejo ambiental"},
+    {name: "Plan de cierre y post cierre de proyectos", text:"Hola, deseo más información sobre el curso de Plan de cierre y post cierre de proyectos"},
+    {name: "Calidad ambiental, evaluación de riesgos y técnicas de remediación ambiental", text:"Hola, deseo más información sobre el curso de Calidad ambiental, evaluación de riesgos y técnicas de remediación ambiental"},
+    
+];
+
+export const coursesGestionSolidos = [
+  
+    {name: "Gestión y manejo de residuos sólidos en minería", text:"Hola, deseo más información sobre el curso de Gestión y manejo de residuos sólidos en minería"},
+    {name: "Gestión y manejo de residuos sólidos de construcción", text:"Hola, deseo más información sobre el curso de Gestión y manejo de residuos sólidos de construcción "},
+    {name: "Formulación de proyectos de inversión pública de residuos sólidos", text:"Hola, deseo más información sobre el curso de Formulación de proyectos de inversión pública de residuos sólidos "},
+    {name: "Gestión y manejo de residuos sólidos industriales", text:"Hola, deseo más información sobre el curso de Gestión y manejo de residuos sólidos industriales"},
+    {name: "Tratamiento y reaprovechamiento de residuos sólidos", text:"Hola, deseo más información sobre el curso de Tratamiento y reaprovechamiento de residuos sólidos"},
+    {name: "Gestión y manejo de residuos sólidos hospitalarios ", text:"Hola, deseo más información sobre el curso de Gestión y manejo de residuos sólidos hospitalarios"},
+    {name: "Diseño de Rellenos sanitarios", text:"Hola, deseo más información sobre el curso de Diseño de Rellenos sanitarios"},
+    {name: "Residuos sólidos y economía circular", text:"Hola, deseo más información sobre el curso de Residuos sólidos y economía circular"},
+    {name: "Operatividad de las EO-RS", text:"Hola, deseo más información sobre el curso de Diseño de Operatividad de las EO-RS"},
+    {name: "Gestión y manejo de residuos sólidos municipales", text:"Hola, deseo más información sobre el curso de Gestión y manejo de residuos sólidos municipales"},
+    {name: "Estudio de caracterización de residuos sólidos", text:"Hola, deseo más información sobre el curso de Estudio de caracterización de residuos sólidos"},
+    {name: "Valorización de residuos sólidos", text:"Hola, deseo más información sobre el curso de Estudio de Valorización de residuos sólidos"},
+
+    
+];
+
+
+export const coursesMonitoreoCalidad = [
+  
+    {name: "Monitoreo y evaluación de la calidad de aire", text:"Hola, deseo más información sobre el curso de Monitoreo y evaluación de la calidad de aire"},
+    {name: "Monitoreo y evaluación de ruido ambiental", text:"Hola, deseo más información sobre el curso de Monitoreo y evaluación de ruido ambiental"},
+    {name: "Monitoreo y evaluación de la calidad de suelo", text:"Hola, deseo más información sobre el curso de Monitoreo y evaluación de la calidad de suelo"},
+    {name: "Monitoreo y evaluación de radiaciones no ionizantes", text:"Hola, deseo más información sobre el curso de Monitoreo y evaluación de radiaciones no ionizantes"},
+    {name: "Monitoreo de agentes ocupacionales", text:"Hola, deseo más información sobre el curso de Monitoreo de agentes ocupacionales"},
+    {name: "Monitoreo biológico ", text:"Hola, deseo más información sobre el curso de Monitoreo biológico"},
+    {name: "Calibración de equipos de monitoreo ambiental ", text:"Hola, deseo más información sobre el curso de Calibración de equipos de monitoreo ambiental "},
+    {name: "Interpretación de informes de laboratorios", text:"Hola, deseo más información sobre el curso de Interpretación de informes de laboratorios"},
+    {name: "Monitoreo y evaluación de la calidad de agua", text:"Hola, deseo más información sobre el curso de Monitoreo y evaluación de la calidad de agua"},
+
+
+    
+];
+  
+
+/* INGENIERIA AGRÓNOMA */
+
+export const coursesFertirriego = [
+
+    {name: "Nutrición vegetal y principios de fertirriego", text:"Hola, deseo más información sobre el curso de Nutrición vegetal y principios de fertirriego"},
+    {name: "Riego tecnificado por aspersión", text:"Hola, deseo más información sobre el curso de riego tecnificado por aspersión"},
+    {name: "Riego tecnificado por goteo", text:"Hola, deseo más información sobre el curso de Riego tecnificado por goteo"},
+    {name: "Fertirrigación ", text:"Hola, deseo más información sobre el curso de Fertirrigación"},
+    {name: "Fertirriego de cultivos y monitoreo ", text:"Hola, deseo más información sobre el curso de Fertirriego de cultivos y monitoreo"}, 
+    {name: "Producción de cultivos sin suelo ", text:"Hola, deseo más información sobre el curso de Producción de cultivos sin suelo"},
+    {name: "Mantenimiento del sistema de riego ", text:"Hola, deseo más información sobre el curso de Mantenimiento del sistema de riego"},
+    {name: "Interpretación y análisis de suelos", text:"Hola, deseo más información sobre el curso de Interpretación y análisis de suelos"},
+    {name: "Calidad de agua para riego ", text:"Hola, deseo más información sobre el curso de Calidad de agua para riego"},
+
+];
+
+export const coursesSistemaRiego = [
+     
+    {name: "Sistemas de riego tecnificado por goteo", text:"Hola, deseo más información sobre el curso de Nutrición vegetal y principios de fertirriego"},
+    {name: "Fertiirrigación ", text:"Hola, deseo más información sobre el curso de Nutrición vegetal y principios de fertirriego"},
+    {name: "Análisis de suelo, agua y foliar", text:"Hola, deseo más información sobre el curso de Análisis de suelo, agua y foliar"},
+    {name: "Importancia del suelo en la agricultura", text:"Hola, deseo más información sobre el curso de Importancia del suelo en la agricultura"},
+    {name: "Hidráulica de tuberías", text:"Hola, deseo más información sobre el curso de Hidráulica de tuberías"},
+    {name: "Descarga de data y parámetros geomorfológicos de la cuenca", text:"Hola, deseo más información sobre el curso de Descarga de data y parámetros geomorfológicos de la cuenca"},
+    {name: "Calidad del agua para riego", text:"Hola, deseo más información sobre el curso de Calidad del agua para riego"},
+    {name: "Oferta y demanda hídrica en cultivos", text:"Hola, deseo más información sobre el curso de Oferta y demanda hídrica en cultivos"},
+    {name: "Sistemas de riego tecnificado por aspersión", text:"Hola, deseo más información sobre el curso de Sistemas de riego tecnificado por aspersión"},
+
+    
+ ]
+
+
+ /* INGENIERIA ALIMENTARIA */
+
+ export const coursesGestionAlimentaria = [
+     
+    {name: "Higiene, Saneamiento y control de plagas en la industria alimentaria ", text:"Hola, deseo más información sobre el curso de Higiene, Saneamiento y control de plagas en la industria alimentaria "},
+    {name: "Interpretación e implementación de la norma ISO 31000 -Gestión de Riesgos", text:"Hola, deseo más información sobre el curso de Interpretación e implementación de la norma ISO 31000 - Gestión de Riesgos"},
+    {name: "Programas Pre-requisitos con base en ISO 22002-1 y BRC v.09", text:"Hola, deseo más información sobre el curso de Programas Pre-requisitos con base en ISO 22002-1 y BRC v.09"},
+    {name: "Análisis de peligros y puntos críticos de control (HACCP)", text:"Hola, deseo más información sobre el curso de Análisis de peligros y puntos críticos de control (HACCP)"},
+    {name: "Esquema FSSC 22000", text:"Hola, deseo más información sobre el curso de Esquema FSSC 22000"},
+    {name: "Fundamentos e Interpretación de los Sistemas de Gestión de la Calidad ISO 9001", text:"Hola, deseo más información sobre el curso de Fundamentos e Interpretación de los Sistemas de Gestión de la Calidad ISO 9001"},
+    {name: "Inspección y muestreo de alimentos", text:"Hola, deseo más información sobre el curso de Inspección y muestreo de alimentos"},
+    {name: "Trazabilidad y etiquetado", text:"Hola, deseo más información sobre el curso de Trazabilidad y etiquetado"},
+    {name: "Auditoria del Sistema de Gestión de Calidad e Inocuidad Alimentaria en base a la Norma ISO 19011", text:"Hola, deseo más información sobre el curso de Auditoria del Sistema de Gestión de Calidad e Inocuidad Alimentaria en base a la Norma ISO 19011"},
+    {name: "Calidad e Inocuidad alimentaria", text:"Hola, deseo más información sobre el curso de Calidad e Inocuidad alimentaria"},
+   
+ ];
+
+
+ /* PROFESIONES EN GENERAL */
+
+ export const coursesProfesionalGeneral = [
+     
+    {name: "Investigación y reporte de accidentes laborales", text:"Hola, deseo más información sobre el curso de Investigación y reporte de accidentes laborales"},
+    {name: "Prevención de riesgos laborales", text:"Hola, deseo más información sobre el curso de Prevención de riesgos laborales"},
+    {name: "Fiscalización y auditorias de SSO - SUNAFIL", text:"Hola, deseo más información sobre el curso de Fiscalización y auditorias de SSO - SUNAFIL"},
+    {name: "Fiscalización ambiental", text:"Hola, deseo más información sobre el curso de Fiscalización ambiental"},
+    {name: "Supervisor SSOMA", text:"Hola, deseo más información sobre el curso de Supervisor SSOMA"},
+    {name: "Auditor Interno SSOMA", text:"Hola, deseo más información sobre el curso de Auditor Interno SSOMA"},
+    {name: "Gestión ambiental - ISO 14001", text:"Hola, deseo más información sobre el curso de Gestión ambiental - ISO 14001"},
+    {name: "Legislación en seguridad y salud en el trabajo", text:"Hola, deseo más información sobre el curso de Legislación en seguridad y salud en el trabajo"},
+    {name: "Implementación de sistema de gestión de SST", text:"Hola, deseo más información sobre el curso de Implementación de sistema de gestión de SST"},
+   
+ ];
